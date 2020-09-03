0cc0b095a7d7d1188c944834b28fb383184b76e2
Your Name <you@example.com>
2020-09-03 15:25:23 +0000

    test

--- trans/product/api.py
+++ trans/product/api.py
@@ -1,15 +1,21 @@
 #Product service
 from flask import Flask
 from flask_restful import Resource, Api
+from googletrans import Translator, constants
+from pprint import pprint
 app = Flask(__name__)
 api = Api(app)
+
+translator = Translator()
+translation = translator.translate("Hola Mundo")
 
 class Product(Resource):
   def get(self):
     return{
       'product': ['Ice cream',
                   'Chocolate',
-                  'Fruit1']
+                  'Fruit1',
+                  translation.text]
           }
 api.add_resource(Product,'/')
 
